29bf92a5a989f867e03e4a96a8dd5d4fcadae3ed
Montassar <@outlook.com>
2023-09-07 10:52:24 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - this is the main function in C language
+ *
+ *Return: 0 to end function in c
+ */
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+}
